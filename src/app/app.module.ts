ef0d7251116d7a0837c08b0bb42983f4590138b1
vinod <shorifali501@gmail.com>
2019-01-03 16:54:26 +0530

    new components resume and front-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    ResumeComponent
   ],
   imports: [
     BrowserModule
